5bc4a8763b7b4115f192f2f490b4ae0aadae8d0b
BuildTools <wjs253@cornell.edu>
2024-02-14 01:54:17 -0500

    fix overflows

--- components/Extra/index.tsx
+++ components/Extra/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Extra = () => {
   return (
     <div
-      className="relative"
+      className="relative overflow-hidden max-w-full"
       style={{minWidth: 450 + 'px'}}
     >
       <div className="absolute left-1/2 -translate-x-2/4 -z-50 w-full">
@@ -12,20 +12,20 @@ const Extra = () => {
           alt="logo"
           width={1200}
           height={600}
-          className="w-full h-auto block"
+          className="w-full h-auto block overflow-hidden"
           style={{minWidth: 600 + 'px'}}
         />
       </div>
       <div className="absolute w-full h-full opacity-60 bg-black -z-10"></div>
       <div className="flex justify-end">
         <div
-          className="w-full text-right max-w-lg p-10"
+          className="w-full text-right max-w-2xl p-10"
           data-wow-delay=".1s"
         >
-          <h2 className="mb-4 text-3xl font-bold !leading-tight text-white sm:text-3xl md:text-4xl">
+          <h2 className="mb-4 text-l font-bold !leading-tight text-white sm:text-2xl md:text-3xl xl:text-5xl">
             Empowering Healthcare Specialists with Financial Growth and Practice Efficiency
           </h2>
-          <p className="text-base !leading-relaxed text-white md:text-lg">
+          <p className="text-base !leading-relaxed text-white md:text-lg xl:text-xl">
             Wood-Works leverages industry expertise to provide customized solutions tailored to unlock new revenue streams, increase practice efficiencies, and empower the passion of caring medical professionals.
           </p>
         </div>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,21 +7,21 @@ const Hero = () => {
         id="home"
         className="bg-white relative z-10 overflow-hidden"
       >
-        <video autoPlay muted loop id="myVideo" className="absolute left-1/2 -translate-x-2/4 -z-50 mt-16 max-w-none w-[1500px] md:w-[1500px] xl:w-[1800px] 2xl:w-[2200px]">
+        <video playsInline autoPlay muted loop id="myVideo" className="absolute left-1/2 -translate-x-2/4 -z-50 mt-16 max-w-none w-[1400px] md:w-[1500px] xl:w-[1800px] 2xl:w-[2200px]">
           <source src="videos/doctors.mp4" type="video/mp4" />
         </video>
         <div className="absolute w-full h-full opacity-60 bg-black -z-10"></div>
-        <div className="container pb-[100px] pt-[300px] md:pb-[140px] md:pt-[340px] xl:pb-[180px] xl:pt-[380px] 2xl:pb-[220px] 2xl:pt-[420px]">
+        <div className="container pb-[80px] pt-[220px] md:pb-[140px] md:pt-[340px] xl:pb-[180px] xl:pt-[380px] 2xl:pb-[220px] 2xl:pt-[420px]">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 font-bold text-3xl leading-tight text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                <h1 className="mb-5 font-bold text-xl leading-tight text-white sm:text-4xl sm:leading-tight sm:text-2xl md:text-4xl md:leading-tight">
                   Empowering medical groups through ancillary services
                 </h1>
-                <p className="text-body-color-dark mb-12 text-white !leading-relaxed text-body-color sm:text-lg md:text-xl">
+                <p className="mb-12 text-white !leading-relaxed text-body-color text-md sm:text-lg md:text-xl">
                   As an authorized medical equipment dealer, we specialize in increasing ancillary revenue for medical professionals through exceptional patient care services.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
